359e5ce1c71a34da23ef2c3a9e76ccab35dfe174
Jake Pressman <jpress7639@gmail.com>
2020-09-03 12:25:28 -0400

    react architecture set

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import Header from './Header/Header';
 
 function App() {
   return (
 <>
 <nav>
+  <h1>Anne Slovin Voice Studio</h1>
   <ul>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
-    <li></li>
+    <li>About</li>
+    <li>Philosophy</li>
+    <li>Rates/Policies</li>
+    <li>Testimonials</li>
+    <li>Contact</li>
   </ul>
 </nav>
+<Header/>
 </>
   );
 }
---
+++ client/src/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function Header(){
+    return(
+        <>
+        <img src="#" alt="#"/>
+        <h2>Let's sing together!</h2>
+        </>
+    )
+}
+
+
+        
+    
+
+
