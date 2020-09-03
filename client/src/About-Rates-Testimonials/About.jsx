314927dbec41935cb3d84b85df6eaba6150ca7e7
Jake Pressman <jpress7639@gmail.com>
2020-09-03 13:59:30 -0400

    setup react architecture

---
+++ client/src/About-Rates-Testimonials/About.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+class About{
+    render() {
+        return(
+            <>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A, eligendi quibusdam. Quae veritatis nisi dolore delectus iste corporis eveniet, recusandae excepturi vel dolorum assumenda, fugit alias asperiores! Magnam, cum hic!</p>
+            </>
+        )
+    }
+}
+
+export default About
---
+++ client/src/About-Rates-Testimonials/Rates.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+class Rates{
+    render() {
+        return(
+            <>
+            </>
+        )
+    }
+}
+
+export default Rates
